c64d4db1cdd505d1cf2621f907d2ad6a9890ec6b
yaffa7 <bmcmahon2@mail.csuchico.edu>
2018-06-16 14:27:35 -0700

    added message count and clear button

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const ADDRESS = 'localhost'
 const socket = new WebSocket(`ws://${ADDRESS}:${PORT}`)
 
 window.onload = function() {
-
     document.getElementById('submitBtn').addEventListener('click', function() {
         let text = document.getElementById('messageInput').value.trim()
         socket.send(text)
+    })  
+    document.getElementById('clearBtn').addEventListener('click', function() {
+        document.getElementById('serverText').innerText = ''
+        let numOfMessages = document.getElementById('serverText').value.split('\n').length
+        document.getElementById('messageCount').innerText = `(${numOfMessages - 1 })`
     })
-    
 }
 
 // When a connection is opened
@@ -20,4 +23,7 @@ socket.addEventListener('open', function(event) {
 // Listen for messages from server
 socket.addEventListener('message', function(event) {
     document.getElementById('serverText').innerText = event.data + '\n' + document.getElementById('serverText').value
+
+    let numOfMessages = document.getElementById('serverText').value.split('\n').length
+    document.getElementById('messageCount').innerText = `(${numOfMessages - 1 })`
 })
