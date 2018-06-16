716cf18b999f1593c863a9a2e0394f3375c83a87
yaffa7 <bmcmahon2@mail.csuchico.edu>
2018-06-16 14:29:25 -0700

    fixed formatting

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const WebSocket = require('ws')
-
 const wss = new WebSocket.Server({ port: 8080 })
 
 wss.on('connection', function connection(ws, req) {
     ws.on('message', function incoming(message) {
         ws.send(`received message: ${message} from client!`)
-        console.log(`IP adress from client ${req.connection.remoteAddress}`)
+        console.log(`IP address from client ${req.connection.remoteAddress}`)
     })
     ws.send(`Server has opened a connection ${ new Date().toLocaleString() }`)
 })
